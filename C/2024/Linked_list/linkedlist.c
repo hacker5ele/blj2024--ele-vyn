0d457692946ab05f18c0a4a242b7a0b09e9a2a32
Eleonora Vynogradova <eleonora.vynogradova@edu.tbz.ch>
2024-11-07 15:00:11 +0100

    Linked list update

--- C/2024/Linked_list/linkedlist.c
+++
@@ -1,5 +0,0 @@
-//
-// Created by Eleonora on 06.11.2024.
-//
-
-#include "linkedlist.h"
--- C/2024/Linked_list/main.c
+++ C/2024/Linked_list/main.c
@@ -1,15 +1,48 @@
 #include <stdio.h>
-#include "linkedlist.h"
 #include <stdlib.h>
 
+typedef struct Node {
+    int data;
+    struct Node *next;
+}TNode;
+
+void printList(TNode *p_head) {
+    TNode *p_cursor = p_head;
+    while (p_cursor != NULL) {  // Loop until reaching the end of the list
+        printf("%d\n", p_cursor->data);  // Print the data of the current node
+        p_cursor = p_cursor->next;       // Move to the next node
+    }
+}
+
+int main(void) {
+
+    TNode *p_head = (TNode*)malloc(sizeof(TNode));
+    p_head->data = 3;
+    p_head->next = NULL;
+    TNode *p_new = (TNode*)malloc(sizeof(TNode));
+    p_new->data = 7;
+    p_new->next = NULL;
+    TNode *p_new2 = (TNode*)malloc(sizeof(TNode));
+    p_new2->data = 5;
+    p_new2->next = NULL;
+    TNode *p_new3 = (TNode*)malloc(sizeof(TNode));
+    p_new3->data = 14;
+    p_new3->next = NULL;
+
+    p_head->next = p_new;
+    p_new->next = p_new2;
+    p_new2->next = p_new3;
+
+    printList(p_head);
 
 
-int main(void)
-{
-   TNode *p_head = (TNode*)malloc(sizeof(TNode));
-   p_head->data = 3;
-   p_head->next = NULL;
-   TNode *p_new = (TNode*)malloc(sizeof(TNode));
-   p_new->data = 7;
-   p_new->next = NULL;
+
+    printf("%d\n", p_head->data);
+    printf("%d\n", p_new->data);
+    printf("%d\n", p_new2->data);
+    printf("%d\n", p_new3->data);
+    printf("%d\n", p_new3->next);
+    printf("%d\n", p_head->next->next->next->data);
+
+    return 0;
 }
